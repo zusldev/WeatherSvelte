b4aa67a5dfedbad74a9c4e573cf011a8889ab3d5
Liborzdev <liborio07zs@gmail.com>
2023-03-15 00:36:27 -0700

    Added custom background
    The background changes automatically based on the day's information from the API.

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,34 @@
-<script>
+<script lang="ts">
     import CardWeather from "./lib/CardWeather.svelte";
     import FeelsLike from "./lib/FeelsLike.svelte";
     import Weather from "./lib/Weather.svelte";
     import Profile from "./lib/Profile.svelte";
+    import { onMount } from "svelte";
+    import { getWeatherData } from "./api/WeatherApi";
+    import type interfaceWeather from "./interfaces/interfaceWeather";
+
+    let weatherData: interfaceWeather;
+    let is_day: number;
+
+    onMount(async () => {
+        weatherData = await getWeatherData();
+        is_day = weatherData.current.is_day;
+    });
 </script>
 
-<html lang="">
-    <body class="bg-black/90">
-        <main class="p-5 2xl:p-10">
-            <div class="flex justify-between">
-                <div class="info">
-                    <CardWeather />
-                </div>
-                <div class="iconDay">
-                    <Weather />
-                </div>
-            </div>
-            <div class="feelslike mt-16">
-                <FeelsLike />
-            </div>
-            <div class="createdBy mt-10">
-                <Profile githubUsername="zusldev" />
-            </div>
-        </main>
-    </body>
-</html>
+<main class="p-5 2xl:p-10 bg-cover {!weatherData ? 'bg-day' : 'bg-night'}">
+    <div class="flex justify-between">
+        <div class="info">
+            <CardWeather />
+        </div>
+        <div class="iconDay">
+            <Weather />
+        </div>
+    </div>
+    <div class="feelslike mt-16">
+        <FeelsLike />
+    </div>
+    <div class="createdBy mt-10">
+        <Profile githubUsername="zusldev" />
+    </div>
+</main>
